b5afda5b0710f509450d25c3c6947daa99420b96
desoares <fabio.desoares@gmail.com>
2018-12-14 17:08:02 -0200

    fixes on .gitignore and README

---
+++ app/build/precache-manifest.370291c4e9f2b9f31ee72f2787a51413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b7ca0e9a51f3065770e",
+    "url": "/static/js/main.8b7ca0e9.chunk.js"
+  },
+  {
+    "revision": "0d6e9d24b6bb6d6529e8",
+    "url": "/static/js/1.0d6e9d24.chunk.js"
+  },
+  {
+    "revision": "8b7ca0e9a51f3065770e",
+    "url": "/static/css/main.47ab326d.chunk.css"
+  },
+  {
+    "revision": "dfd45e4e2a52298c25df5492377df3c7",
+    "url": "/index.html"
+  }
+];
